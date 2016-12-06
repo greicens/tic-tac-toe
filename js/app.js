2f2df1d5036d981e518fff525758f825303250c4
greicens <gsilva@mail.ccsf.edu>
2016-12-05 18:33:20 -0800

    created alert if the space is already taken

--- js/app.js
+++ js/app.js
@@ -1,76 +1,40 @@
 // wait for the DOM to finish loading
 console.log("javaScript is loaded");
 $(document).ready(function() {
+  $(".message").text("Player X starts");
   $("#board").on("click", markBoard);
+  $(".button").click(function() {
+   location.reload();
+ });
 });
+var playerX = true;
+var playerO = false;
+var elementClicked =[];
 
-function markBoard(){
+
+function markBoard(event){
   var whichSquare = $(event.target).get(0);
-  console.log(whichSquare);
-  var playerX = true;
-  var playerO = false;
+  console.log($(whichSquare).text().length)
+  //elementClicked.push(whichSquare);
 
-  while(isBoardFull === false){
+  if($(whichSquare).text().length > 0){
+    alert("this is space is taken");
+  }else{
+
    if(playerX === true){
-     console.log("playerx")
+     $(".message").text("Player O turn ");
      $(event.target).text("X");
+     playerX = false;
+     playerO = true;
+   }else if(playerX === false){
      $(".message").text("Player X turn ");
-
-    playerX = false;
-    playerO = true;
-  }
-  if(playerX === false){
-     console.log("playerO turn")
-     $(".message").text("Player X turn ");
-
-    $(event.target).text("O");
-    playerX = true;
-    playerO = false;
-
+     $(event.target).text("O");
+     playerX = true;
+     playerO = false;
    }
  }
-    //getPlayer();
-  //}
-  //if(getPlayer() === false){
-    //$(event.target).text("O");
-  //  getPlayer();
-
-  //}
-  //$(event.target).text("X")
-//  $(".message").append("winner!");
-
 }
 
-
-// function getPlayer(){
-//   console.log("this is changePlayerfunction")
-//   if(!isBoardFull()){
-//     // console.log("is board not full : " + !isBoardFull());
-//     var turn = true;
-//
-//     if(turn === true){
-//       console.log("Player X turn");
-//       $(".message").text("Player X turn ");
-//       turn = false;
-//       return true;
-//    }
-//     if( turn === false){
-//       console.log("player O turn");
-//       $(event.target).text("O");
-//       $(".message").text("Player O turn ");
-//       turn = true;
-//
-//       return false;
-//
-//     }
-//    }
-//  }
-//
-//
-// function winner(){
-//
-// }
-//
 function isBoardFull(){
   console.log("isBoardFullfunction")
   if($("#board").text().length === 227){
